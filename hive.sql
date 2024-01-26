660395e1676e8b3e5bc61074a21e5a82207448a6
r3ps4J <40723838+r3ps4J@users.noreply.github.com>
2024-01-26 17:43:22 +0100

    Revert "Create database if not exists"
    This reverts commit 4ede947c888ea42e0a71f2500b1caf20bdd06acc.

--- hive.sql
+++ hive.sql
@@ -19,9 +19,6 @@
 -- Table structure for table `games`
 --
 
-CREATE DATABASE IF NOT EXISTS `hive`;
-USE `hive`;
-
 DROP TABLE IF EXISTS `games`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
