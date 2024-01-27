34cf9938d492783c80248fc2fdadcb917dda4acb
r3ps4J <40723838+r3ps4J@users.noreply.github.com>
2024-01-27 15:30:03 +0100

    Format initial hand array

--- src/restart.php
+++ src/restart.php
@@ -3,7 +3,22 @@
 session_start();
 
 $_SESSION['board'] = [];
-$_SESSION['hand'] = [0 => ["Q" => 1, "B" => 2, "S" => 2, "A" => 3, "G" => 3], 1 => ["Q" => 1, "B" => 2, "S" => 2, "A" => 3, "G" => 3]];
+$_SESSION['hand'] = [
+    0 => [
+        "Q" => 1,
+        "B" => 2,
+        "S" => 2,
+        "A" => 3,
+        "G" => 3,
+    ],
+    1 => [
+        "Q" => 1,
+        "B" => 2,
+        "S" => 2,
+        "A" => 3,
+        "G" => 3,
+    ],
+];
 $_SESSION['player'] = 0;
 
 $db = include_once 'database.php';
