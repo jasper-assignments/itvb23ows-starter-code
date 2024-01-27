63a168c15487a8bfb20648e00404c56a23c69051
r3ps4J <40723838+r3ps4J@users.noreply.github.com>
2024-01-27 14:40:45 +0100

    Replace "include" with "include_once"

--- src/index.php
+++ src/index.php
@@ -167,7 +167,7 @@
         <strong><?php if (isset($_SESSION['error'])) echo($_SESSION['error']); unset($_SESSION['error']); ?></strong>
         <ol>
             <?php
-                $db = include 'database.php';
+                $db = include_once 'database.php';
                 $stmt = $db->prepare('SELECT * FROM moves WHERE game_id = '.$_SESSION['game_id']);
                 $stmt->execute();
                 $result = $stmt->get_result();
