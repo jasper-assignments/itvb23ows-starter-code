741e067572114c78063f9817d6a2475ab67b4884
r3ps4J <40723838+r3ps4J@users.noreply.github.com>
2024-02-01 22:00:30 +0100

    Add check preventing spider from moving to occupied positions

--- src/Piece/Spider.php
+++ src/Piece/Spider.php
@@ -6,6 +6,12 @@ class Spider extends AbstractPiece
 {
     public function isMoveValid(string $from, string $to): bool
     {
+        $board = clone $this->board;
+        $board->popTile($from);
+
+        if (!$board->isPositionEmpty($to)) {
+            return false;
+        }
         return true;
     }
 }
