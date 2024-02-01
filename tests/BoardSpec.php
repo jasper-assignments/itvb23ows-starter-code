322f9db23ded88edd7627ce3999f29a2d08817fa
r3ps4J <40723838+r3ps4J@users.noreply.github.com>
2024-02-01 12:07:54 +0100

    Add test to BoardSpec to test straight move on downward diagonal

--- tests/BoardSpec.php
+++ tests/BoardSpec.php
@@ -65,4 +65,19 @@ class BoardSpec extends TestCase
         // assert
         $this->assertTrue($valid);
     }
+
+    #[Test]
+    public function givenGrasshopperStraightDestinationOnDownwardDiagonalThenMoveValidIsTrue()
+    {
+        // arrange
+        $board = new Board();
+        $from = '0,0';
+        $to = '2,-2';
+
+        // act
+        $valid = $board->isGrasshopperMoveValid($from, $to);
+
+        // assert
+        $this->assertTrue($valid);
+    }
 }
