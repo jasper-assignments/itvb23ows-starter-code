05a59f5325ad9b8ce1acfe8fa1c5bf96312ea1f8
r3ps4J <40723838+r3ps4J@users.noreply.github.com>
2024-02-01 15:55:00 +0100

    Add check to prevent soldier from moving to current position

--- src/Piece/SoldierAnt.php
+++ src/Piece/SoldierAnt.php
@@ -7,6 +7,10 @@ class SoldierAnt extends AbstractPiece
 
     public function isMoveValid(string $from, string $to): bool
     {
+        if ($from == $to) {
+            $this->setErrorMessage('Tile must move');
+            return false;
+        }
         return true;
     }
 }
