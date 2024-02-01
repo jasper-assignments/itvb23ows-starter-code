e7d7f5e9378c07d3ea5059348a39bd3c7480ee47
r3ps4J <40723838+r3ps4J@users.noreply.github.com>
2024-02-01 15:59:41 +0100

    Add check to soldier to prevent moving to occupied position

--- src/Piece/SoldierAnt.php
+++ src/Piece/SoldierAnt.php
@@ -7,8 +7,14 @@ class SoldierAnt extends AbstractPiece
 
     public function isMoveValid(string $from, string $to): bool
     {
+        $board = clone $this->board;
+        $board->popTile($from);
+
         if ($from == $to) {
             $this->setErrorMessage('Tile must move');
+            return false;
+        } elseif (!$board->isPositionEmpty($to)) {
+            $this->setErrorMessage('Tile not empty');
             return false;
         }
         return true;
