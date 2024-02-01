6a4b5e24f43f856859d8ca683a7f5f80b3937ade
r3ps4J <40723838+r3ps4J@users.noreply.github.com>
2024-02-01 15:53:48 +0100

    Create test for ensuring soldier ant can't move to current position

--- tests/SoldierAntSpec.php
+++ tests/SoldierAntSpec.php
@@ -24,4 +24,22 @@ class SoldierAntSpec extends TestCase
         // assert
         $this->assertTrue($valid);
     }
+
+    #[Test]
+    public function givenCurrentPositionAsDestinationThenMoveValidIsFalse()
+    {
+        // arrange
+        $board = new Board([
+            '0,0' => [[0, 'S']],
+        ]);
+        $soldierAnt = new SoldierAnt($board);
+        $from = '0,0';
+        $to = '0,0';
+
+        // act
+        $valid = $soldierAnt->isMoveValid($from, $to);
+
+        // assert
+        $this->assertFalse($valid);
+    }
 }
