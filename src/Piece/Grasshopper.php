6dd60e16d43072eb0333a19ae838bd3ac516e2b3
r3ps4J <40723838+r3ps4J@users.noreply.github.com>
2024-02-01 15:12:13 +0100

    Add check to grasshopper to see if all positions between from and to are occupied

--- src/Piece/Grasshopper.php
+++ src/Piece/Grasshopper.php
@@ -9,12 +9,15 @@ class Grasshopper extends AbstractPiece
         if (!$this->isMoveStraight($from, $to)) {
             $this->setErrorMessage('Move is not straight');
             return false;
+        } elseif (!$this->areAllPositionsBetweenOccupied($from, $to)) {
+            $this->setErrorMessage('Move jumps over empty positions');
+            return false;
         }
 
         return true;
     }
 
-    public function isMoveStraight(string $from, string $to): bool
+    private function isMoveStraight(string $from, string $to): bool
     {
         [$fromX, $fromY] = explode(',', $from);
         [$toX, $toY] = explode(',', $to);
@@ -30,5 +33,46 @@ class Grasshopper extends AbstractPiece
         }
 
         return false;
+    }
+
+    private function areAllPositionsBetweenOccupied(string $from, string $to): bool
+    {
+        $board = clone $this->board;
+        $board->popTile($from);
+
+        $positions = $this->getCoordinatesBetween($from, $to);
+        foreach ($positions as $pos) {
+            if ($board->isPositionEmpty($pos)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private function getCoordinatesBetween(string $start, string $end): array
+    {
+        [$startX, $startY] = explode(',', $start);
+        [$endX, $endY] = explode(',', $end);
+
+        $coordinates = [];
+        if ($startY == $endY) { // Horizontal
+            for ($x = min($startX, $endX) + 1; $x < max($startX, $endX); $x++) {
+                $coordinates[] = "$x,$startY";
+            }
+        } elseif ($startX == $endX) { // Vertical
+            for ($y = min($startY, $endY) + 1; $y < max($startY, $endY); $y++) {
+                $coordinates[] = "$startX,$y";
+            }
+        } else {
+            $slope = ($endY - $startY) / ($endX - $startX);
+            $intercept = $startY - $slope * $startX;
+            for ($x = $startX + 1; $x < $endX; $x++) {
+                $y = $slope * $x + $intercept;
+                $coordinates[] = "$x,$y";
+            }
+        }
+
+        return $coordinates;
     }
 }
