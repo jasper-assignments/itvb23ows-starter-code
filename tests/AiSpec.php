c9c44a260f4f7633efee85cde12a70c51e3e1664
r3ps4J <40723838+r3ps4J@users.noreply.github.com>
2024-02-02 17:31:03 +0100

    Add test to AiSpec ensuring hand pieces get added to body correctly

--- tests/AiSpec.php
+++ tests/AiSpec.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Entity\Ai;
+use App\Entity\Hand;
 use GuzzleHttp\Client;
 use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\TestCase;
@@ -20,5 +21,24 @@ class AiSpec extends TestCase
 
         // assert
         $this->assertSame(1, $body['move_number']);
+    }
+
+    #[Test]
+    public function givenHandsArrayThenBuildBodyAddsPiecesCorrectlyToBody()
+    {
+        // arrange
+        $guzzleClientMock = Mockery::mock(Client::class);
+        $ai = new Ai($guzzleClientMock);
+        $moveNumber = 1;
+        $hands = [
+            0 => new Hand(),
+            1 => new Hand(),
+        ];
+
+        // act
+        $body = $ai->buildBody($moveNumber, $hands);
+
+        // assert
+        $this->assertSame($hands[0]->getPieces(), $body['hand'][0]);
     }
 }
