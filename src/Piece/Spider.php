7d724c178103d36ad566d733872c296ba8a6c788
r3ps4J <40723838+r3ps4J@users.noreply.github.com>
2024-02-02 11:43:53 +0100

    Add slide function to Spider

--- src/Piece/Spider.php
+++ src/Piece/Spider.php
@@ -2,6 +2,8 @@
 
 namespace App\Piece;
 
+use App\Entity\Board;
+
 class Spider extends AbstractPiece
 {
     public function isMoveValid(string $from, string $to): bool
@@ -12,7 +14,37 @@ class Spider extends AbstractPiece
         if (!$board->isPositionEmpty($to)) {
             $this->setErrorMessage('Tile not empty');
             return false;
+        } elseif (!$this->canDestinationBeReachedBySliding($board, $from, $to)) {
+            $this->setErrorMessage('Tile must slide');
+            return false;
         }
         return true;
+    }
+
+    public function canDestinationBeReachedBySliding(
+        Board $board,
+        string $current,
+        string $destination,
+        array $visited = []
+    ): bool {
+        if ($current == $destination) {
+            return true;
+        }
+
+        $emptyNeighbours = $board->getNeighbourPositions($current, fn($neighbour) => $board->isPositionEmpty($neighbour));
+        foreach ($emptyNeighbours as $neighbor) {
+            if (in_array($neighbor, $visited)) {
+                continue;
+            }
+
+            if ($board->slide($current, $neighbor)) {
+                $visited[] = $current;
+                if ($this->canDestinationBeReachedBySliding($board, $neighbor, $destination, $visited)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
 }
