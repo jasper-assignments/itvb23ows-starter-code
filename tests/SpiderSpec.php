13c472b048d52f5d4ebfbc2ce17052699364650b
r3ps4J <40723838+r3ps4J@users.noreply.github.com>
2024-02-02 11:43:07 +0100

    Add test to SpiderSpec ensuring moves more than 3 slides away are invalid

--- tests/SpiderSpec.php
+++ tests/SpiderSpec.php
@@ -28,6 +28,26 @@ class SpiderSpec extends TestCase
     }
 
     #[Test]
+    public function givenDestinationMoreThanThreeStepsAwayThenMoveValidIsFalse()
+    {
+        // arrange
+        $board = new Board([
+            '1,-1' => [[0, 'S']],
+            '0,0' => [[0, 'Q']],
+            '1,0' => [[1, 'Q']],
+        ]);
+        $spider = new Spider($board);
+        $from = '1,-1';
+        $to = '0,1';
+
+        // act
+        $valid = $spider->isMoveValid($from, $to);
+
+        // assert
+        $this->assertFalse($valid);
+    }
+
+    #[Test]
     public function givenOccupiedPositionAsDestinationThenMoveValidIsFalse()
     {
         // arrange
